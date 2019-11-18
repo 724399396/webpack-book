f812fe7efd95325fc92db9b6c4ba778b565b86f1
Wei Li <liwei72439936@gmail.com>
2019-11-18 18:40:15 +0800

    style loading

--- webpack.parts.js
+++ webpack.parts.js
@@ -2,9 +2,25 @@ exports.devServer = ({
     host,
     port
 } = {}) => ({
+  devServer: {
     stats: "errors-only",
     host: host,
     port: port,
-    open: true, // Open the page in browser
+    open: true,
     overlay: true
+  }
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 });
